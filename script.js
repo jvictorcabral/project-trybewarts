bef0bb05192718434edf2249ce202c7fb1514e5b
Guilherme Leite <guilhermelax@hotmail.com>
2021-11-09 19:52:10 -0300

    fix: alterando a posiçao do script e finalizando requisito 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('log-in');
+const email = document.getElementById('email');
+const passWord = document.getElementById('password');
+
+btn.addEventListener('click', () => {
+  if (email.value === 'tryber@teste.com' && passWord.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos');
+  }
+});
